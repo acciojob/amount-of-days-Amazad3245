0252b56b6b13c67f0e296f7a8d826084852e62c1
Amazad3245 <amazadweb3@gmail.com>
2024-10-05 19:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    
+    const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+
+    
+    return isLeapYear ? 366 : 365;
+
+}
+
+
+// Output: 365
